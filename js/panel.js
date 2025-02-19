6170e5f0943002cf38db44b05ef37e237263d068
Pvezaa <vitalikkircioglo91@gmail.com>
2025-02-19 23:04:56 +0200

    cupon

--- js/panel.js
+++ js/panel.js
@@ -1,12 +1,9 @@
-try{
-    const token=JSON.parse(localStorage.getItem('accessToken'));
+
+const token=JSON.parse(localStorage.getItem('accessToken'));
     if(!token){
         window.location.href = 'http://127.0.0.1:9092/Coffe/index.html';
   }
-  }
-  catch (error) {
-    console.log('')
-  }
+
 let allitems=[];
 async function Statistiktable(start, end) {
     try{
